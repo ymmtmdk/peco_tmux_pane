0a0483c1522879e2ee062c7b77407452bee407a5
ymmtmdk <ymmtmdk@gmail.com>
2017-03-29 19:04:36 +0900

    Update peco_tmux_pane.fish

--- peco_tmux_pane.fish
+++ peco_tmux_pane.fish
@@ -1,9 +1,6 @@
-
 function peco_tmux_pane
   tmux capture-pane \; show-buffer \; delete-buffer | /usr/bin/ruby -ne 'puts $_, $_.split' | sort | uniq | peco | read res
   if test $res
     commandline -i -- $res
   end
 end
-
-
